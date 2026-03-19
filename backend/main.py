1b17f0fe2c1ce19f21e0f07518699e8fb0d00c48
kukuo143 <coke04052@gmail.com>
2026-03-19 12:22:45 +0900

    Rename hash_password function to password_hash

--- backend/main.py
+++ backend/main.py
@@ -53,7 +53,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash_password(password) -> str:
+def password_hash(password) -> str:
     return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
